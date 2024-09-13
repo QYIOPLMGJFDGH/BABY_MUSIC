b0c576053b7e565349ed95193f73064d9f75c77f
UTTAM RATHORE <uk977061@gmail.com>
2024-09-13 13:27:00 +0530

    Update help.py

--- SONALI/utils/inline/help.py
+++ SONALI/utils/inline/help.py
@@ -19,72 +19,72 @@ def help_pannel(_, START: Union[bool, int] = None):
         [
             [
                 InlineKeyboardButton(
-                    text="• ᴀᴄᴛɪᴠᴇ •",
+                    text="H_B_1",
                     callback_data="help_callback hb1",
                 ),
                 InlineKeyboardButton(
-                    text="• ᴀᴅᴍɪɴ •",
+                    text="H_B_2",
                     callback_data="help_callback hb2",
                 ),
                 InlineKeyboardButton(
-                    text="• ᴀᴜᴛʜ •",
+                    text="H_B_3",
                     callback_data="help_callback hb3",
                 ),
             ],
             [
                 InlineKeyboardButton(
-                    text="• ʙʟᴏᴄᴋ •",
+                    text="H_B_4",
                     callback_data="help_callback hb4",
                 ),
                 InlineKeyboardButton(
-                    text="• ʙᴏᴛ •",
+                    text="H_B_5",
                     callback_data="help_callback hb5",
                 ),
                 InlineKeyboardButton(
-                    text="• ᴅᴇᴠ •",
+                    text="H_B_6",
                     callback_data="help_callback hb6",
                 ),
             ],
             [
                 InlineKeyboardButton(
-                    text="• ʙʟᴏᴄᴋ •",
-                    callback_data="help_callback hb4",
-                ),
-                InlineKeyboardButton(
-                    text="• ʙᴏᴛ •",
-                    callback_data="help_callback hb5",
-                ),
-                InlineKeyboardButton(
-                    text="• ᴅᴇᴠ •",
-                    callback_data="help_callback hb6",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text="• ʙʟᴏᴄᴋ •",
-                    callback_data="help_callback hb4",
-                ),
-                InlineKeyboardButton(
-                    text="• ʙᴏᴛ •",
-                    callback_data="help_callback hb5",
-                ),
-                InlineKeyboardButton(
-                    text="• ᴅᴇᴠ •",
-                    callback_data="help_callback hb6",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text="• ɢ-ᴄᴀsᴛ •",
+                    text="H_B_7",
                     callback_data="help_callback hb7",
                 ),
                 InlineKeyboardButton(
-                    text="• ᴘ-ʟɪsᴛ •",
+                    text="H_B_8",
                     callback_data="help_callback hb8",
                 ),
                 InlineKeyboardButton(
-                    text="• ᴘʟᴀʏ •",
+                    text="H_B_9",
                     callback_data="help_callback hb9",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text="H_B_10",
+                    callback_data="help_callback hb10",
+                ),
+                InlineKeyboardButton(
+                    text="H_B_11",
+                    callback_data="help_callback hb11",
+                ),
+                InlineKeyboardButton(
+                    text="H_B_12",
+                    callback_data="help_callback hb12",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text="H_B_13",
+                    callback_data="help_callback hb13",
+                ),
+                InlineKeyboardButton(
+                    text="H_B_14",
+                    callback_data="help_callback hb14",
+                ),
+                InlineKeyboardButton(
+                    text="H_B_15",
+                    callback_data="help_callback hb15",
                 ),
             ], 
             mark,
