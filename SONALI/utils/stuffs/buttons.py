7771df007739e1f26614fabfb9037b724169d33d
UTTAM RATHORE <uk977061@gmail.com>
2024-09-13 15:21:10 +0530

    Update buttons.py

--- SONALI/utils/stuffs/buttons.py
+++ SONALI/utils/stuffs/buttons.py
@@ -15,3 +15,14 @@ class BUTTONS(object):
     InlineKeyboardButton("• ғᴜи •", callback_data="mplus HELP_Fun"),InlineKeyboardButton("• ǫᴜɪʟʏ •", callback_data="mplus HELP_Q")],          
     [InlineKeyboardButton("• ʙᴀᴄᴋ •", callback_data=f"settingsback_helper"), 
     ]]
+
+
+class BUTTONS1(object):
+    MBUTTON1 = [[InlineKeyboardButton("• ᴀɪ •", callback_data="mplus HELP_ChatGPT"),InlineKeyboardButton("• ɪɴғᴏ •", callback_data="mplus HELP_Info"),InlineKeyboardButton("• sᴛɪᴄᴋᴇꝛ •", callback_data="mplus HELP_Sticker")],
+    [InlineKeyboardButton("• ᴇxᴛꝛᴧ •", callback_data="mplus HELP_Extra"),
+    InlineKeyboardButton("• ɪᴍᴧɢᴇ •", callback_data="mplus HELP_Image"),InlineKeyboardButton("• sᴇᴧꝛᴄʜ •", callback_data="mplus HELP_Search")],
+    [InlineKeyboardButton("• ǫᴜɪʟʏ •", callback_data="mplus HELP_Q")],
+    [InlineKeyboardButton("• ғᴏиᴛ •", callback_data="mplus HELP_Font"),
+    InlineKeyboardButton("• ɢᴧᴍᴇ •", callback_data="mplus HELP_Game"),InlineKeyboardButton("• ᴛ-ɢꝛᴧᴘʜ •", callback_data="mplus HELP_TG")],    
+    [InlineKeyboardButton("• ʙᴀᴄᴋ •", callback_data=f"settingsback_helper"), 
+    ]]
