fc1a7c71e4882712fc686aff12554aa9a3b5100a
UTTAM RATHORE <uk977061@gmail.com>
2024-09-13 16:15:36 +0530

    Update buttons.py

--- SONALI/utils/stuffs/buttons.py
+++ SONALI/utils/stuffs/buttons.py
@@ -2,17 +2,12 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram import Client, filters, enums 
 
 class BUTTONS(object):
-    BABYTOOL = [[InlineKeyboardButton("• ᴄʜᴧᴛ-ɢᴘᴛ •", callback_data="mplus HELP_ChatGPT"),InlineKeyboardButton("• ɢꝛᴏᴜᴘ •", callback_data="mplus HELP_Group"),InlineKeyboardButton("• sᴛɪᴄᴋᴇꝛ •", callback_data="mplus HELP_Sticker")],
+    BABYTOOL = [[InlineKeyboardButton("• ɢꝛᴏᴜᴘ •", callback_data="mplus HELP_Group"),InlineKeyboardButton("• ᴧᴄᴛɪᴏи •", callback_data="mplus HELP_Action"),InlineKeyboardButton("• sᴛɪᴄᴋᴇꝛ •", callback_data="mplus HELP_Sticker")],
     [InlineKeyboardButton("• ᴛᴧɢ-ᴧʟʟ •", callback_data="mplus HELP_TagAll"),
-    InlineKeyboardButton("• ɪɴғᴏ •", callback_data="mplus HELP_Info"),InlineKeyboardButton("• ᴇxᴛꝛᴧ •", callback_data="mplus HELP_Extra")],
-    [InlineKeyboardButton("• ɪᴍᴧɢᴇ •", callback_data="mplus HELP_Image"),
-    InlineKeyboardButton("• ᴧᴄᴛɪᴏи •", callback_data="mplus HELP_Action"),InlineKeyboardButton("• sᴇᴧꝛᴄʜ •", callback_data="mplus HELP_Search")],    
-    [InlineKeyboardButton("• ғᴏиᴛ •", callback_data="mplus HELP_Font"),
-    InlineKeyboardButton("• ɢᴧᴍᴇ •", callback_data="mplus HELP_Game"),InlineKeyboardButton("• ᴛ-ɢꝛᴧᴘʜ •", callback_data="mplus HELP_TG")],
-    [InlineKeyboardButton("• ɪᴍᴘᴏsᴛᴇꝛ •", callback_data="mplus HELP_Imposter"),
-    InlineKeyboardButton("• ᴛʀᴜᴛʜᴅᴧꝛᴇ •", callback_data="mplus HELP_TD"),InlineKeyboardButton("• ʜᴧsʜ-ᴛᴧɢ •", callback_data="mplus HELP_HT")], 
+    InlineKeyboardButton("• ɪᴍᴘᴏsᴛᴇꝛ •", callback_data="mplus HELP_Imposter"),InlineKeyboardButton("• ᴛ-ᴅᴧꝛᴇ •", callback_data="mplus HELP_TD")],
+    [InlineKeyboardButton("• ʜᴧsʜ-ᴛᴧɢ •", callback_data="mplus HELP_HT")],
     [InlineKeyboardButton("• ттѕ •", callback_data="mplus HELP_TTS"),
-    InlineKeyboardButton("• ғᴜи •", callback_data="mplus HELP_Fun"),InlineKeyboardButton("• ǫᴜɪʟʏ •", callback_data="mplus HELP_Q")],          
+    InlineKeyboardButton("• ғᴜи •", callback_data="mplus HELP_Fun")],    
     [InlineKeyboardButton("• ʙᴀᴄᴋ •", callback_data=f"settingsback_helper"), 
     ]]
 
