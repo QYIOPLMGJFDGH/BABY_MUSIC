eb8f5cc1cbe0acafe961216a3f4aec40355784c0
UTTAM RATHORE <uk977061@gmail.com>
2024-09-13 15:12:32 +0530

    Update start.py

--- SONALI/utils/inline/start.py
+++ SONALI/utils/inline/start.py
@@ -25,19 +25,22 @@ def private_panel(_):
             )
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
-          InlineKeyboardButton(text=_["S_B_12"], callback_data=f"mbot_cb"),
+          InlineKeyboardButton(text=_["S_B_12"], callback_data=f"mbot_cb1"),
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_13"], callback_data=f"mbot_cb"),
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-        ],
-        [
             InlineKeyboardButton(text=_["S_B_10"], url="https://t.me/+tHAENx_r_mtlODZl"),
             InlineKeyboardButton(text=_["S_B_11"], url="https://t.me/BABY09_WORLD/118"),
-        ]
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+        ],
         ]
     
     return buttons
