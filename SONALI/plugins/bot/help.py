51014b8c495020b65b36bc9c58235ed8fe1f1d98
UTTAM RATHORE <uk977061@gmail.com>
2024-09-13 15:39:04 +0530

    Update help.py

--- SONALI/plugins/bot/help.py
+++ SONALI/plugins/bot/help.py
@@ -89,7 +89,7 @@ async def helper_cb(client, CallbackQuery, _):
         await CallbackQuery.edit_message_text(helpers.HELP_15, reply_markup=keyboard)
         
 
-@app.on_callback_query(filters.regex("mbot_cb") & ~BANNED_USERS)
+@app.on_callback_query(filters.regex("mbot_cb","mbot_cb1") & ~BANNED_USERS)
 async def helper_cb(client, CallbackQuery):
     await CallbackQuery.edit_message_text(Helper.HELP_M, reply_markup=InlineKeyboardMarkup(BUTTONS.MBUTTON1,BUTTONS.MBUTTON))
     
