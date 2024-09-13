b896d37715105bb6985979923053e988ab582f16
UTTAM RATHORE <uk977061@gmail.com>
2024-09-13 16:27:15 +0530

    Update help.py

--- SONALI/plugins/bot/help.py
+++ SONALI/plugins/bot/help.py
@@ -90,11 +90,11 @@ async def helper_cb(client, CallbackQuery, _):
 
 @app.on_callback_query(filters.regex("mbot_baby") & ~BANNED_USERS)
 async def helper_cb(client, CallbackQuery):
-    await CallbackQuery.edit_message_text(Helper.HELP_M, reply_markup=InlineKeyboardMarkup(BUTTONS.MBUTTON1))
+    await CallbackQuery.edit_message_text(Helper.HELP_M, reply_markup=InlineKeyboardMarkup(BUTTONS.MBUTTON2))
 
 @app.on_callback_query(filters.regex("mbot_cb") & ~BANNED_USERS)
 async def helper_cb(client, CallbackQuery):
-    await CallbackQuery.edit_message_text(Helper.HELP_M, reply_markup=InlineKeyboardMarkup(BUTTONS.MBUTTON2))
+    await CallbackQuery.edit_message_text(Helper.HELP_M, reply_markup=InlineKeyboardMarkup(BUTTONS.MBUTTON1))
     
 
 @app.on_callback_query(filters.regex('managebot123'))
