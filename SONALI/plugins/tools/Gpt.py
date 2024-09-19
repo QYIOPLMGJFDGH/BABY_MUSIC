e9a0872fa6b020953434f54bfad2b4b783c4d30d
UTTAM RATHORE <uk977061@gmail.com>
2024-09-19 12:57:02 +0530

    Update Gpt.py

--- SONALI/plugins/tools/Gpt.py
+++ SONALI/plugins/tools/Gpt.py
@@ -5,7 +5,7 @@ from SONALI import app
 import requests
 from pyrogram.types import  Message
 from pyrogram.types import InputMediaPhoto
-from Fast_GPTai import api
+from teambabyAPI import api
 from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
 
