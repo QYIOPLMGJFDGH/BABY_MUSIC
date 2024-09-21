a378cba3358a33abd9f2c896c8eb9c2a1098e232
UTTAM RATHORE <uk977061@gmail.com>
2024-09-21 16:39:49 +0530

    Update buttons.py

--- SONALI/utils/stuffs/buttons.py
+++ SONALI/utils/stuffs/buttons.py
@@ -8,7 +8,7 @@ class BUTTONS(object):
     [InlineKeyboardButton("• ʜᴧsʜ-ᴛᴧɢ •", callback_data="dplus HELP_HT")],
     [InlineKeyboardButton("• ттѕ •", callback_data="dplus HELP_TTS"),
     InlineKeyboardButton("• ғᴜи •", callback_data="dplus HELP_Fun")],    
-    [InlineKeyboardButton("↺ ʙᴀᴄᴋ ↻", callback_data=f"modebot_cb"), 
+    [InlineKeyboardButton("↺ ʙᴧᴄᴋ ↻", callback_data=f"modebot_cb"), 
     ]]
     
     MBUTTON = [[InlineKeyboardButton("• ᴀɪ •", callback_data="mplus HELP_ChatGPT"),InlineKeyboardButton("• ɪɴғᴏ •", callback_data="mplus HELP_Info"),InlineKeyboardButton("• sᴛɪᴄᴋᴇꝛ •", callback_data="mplus HELP_Sticker")],
@@ -17,14 +17,14 @@ class BUTTONS(object):
     [InlineKeyboardButton("• ǫᴜɪʟʏ •", callback_data="mplus HELP_Q")],
     [InlineKeyboardButton("• ғᴏиᴛ •", callback_data="mplus HELP_Font"),
     InlineKeyboardButton("• ɢᴧᴍᴇ •", callback_data="mplus HELP_Game"),InlineKeyboardButton("• ᴛ-ɢꝛᴧᴘʜ •", callback_data="mplus HELP_TG")],    
-    [InlineKeyboardButton("↺ ʙᴀᴄᴋ ↻", callback_data=f"modebot_cb"), 
+    [InlineKeyboardButton("↺ ʙᴧᴄᴋ ↻", callback_data=f"modebot_cb"), 
     ]]
 
-    ABUTTON = [[InlineKeyboardButton("˹ sᴜᴘᴘᴏʀᴛ ˼", url="https://t.me/+OL6jdTL7JAJjYzVl")],[InlineKeyboardButton("˹ ᴜᴘᴅᴀᴛᴇ ˼", url="https://t.me/BABY09_WORLD"),
-    InlineKeyboardButton("˹ ᴀʟʟ ʙᴏᴛ ˼", url="https://t.me/+tHAENx_r_mtlODZl")],[InlineKeyboardButton("↺ ʙᴀᴄᴋ ↻", callback_data=f"settingsback_helper"),
+    ABUTTON = [[InlineKeyboardButton("˹ sᴜᴘᴘᴏꝛᴛ ˼", url="https://t.me/+OL6jdTL7JAJjYzVl")],[InlineKeyboardButton("˹ ᴜᴘᴅᴧᴛᴇ ˼", url="https://t.me/BABY09_WORLD"),
+    InlineKeyboardButton("˹ ᴧʟʟ ʙᴏᴛ ˼", url="https://t.me/+tHAENx_r_mtlODZl")],[InlineKeyboardButton("↺ ʙᴧᴄᴋ ↻", callback_data=f"settingsback_helper"),
     ]]
 
-    UBUTTON = [[InlineKeyboardButton("˹ ᴍᴜsɪᴄ ˼", callback_data="settings_back_helper"),InlineKeyboardButton("˹ ᴛᴏᴏʟs ˼", callback_data=f"mbot_cb")],[InlineKeyboardButton("˹ ᴍᴀɴᴀɢᴇ ˼", callback_data=f"bbot_cb"),
-    InlineKeyboardButton("˹ ʀᴀɪᴅ ˼", callback_data="cplus HELP_raid")],[InlineKeyboardButton("↺ ʙᴀᴄᴋ ↻", callback_data=f"settingsback_helper"),
+    UBUTTON = [[InlineKeyboardButton("˹ ᴍᴜsɪᴄ ˼", callback_data="settings_back_helper"),InlineKeyboardButton("˹ ᴛᴏᴏʟs ˼", callback_data=f"mbot_cb")],[InlineKeyboardButton("˹ ᴍᴧɴᴧɢᴇ ˼", callback_data=f"bbot_cb"),
+    InlineKeyboardButton("˹ ʀᴧɪᴅ ˼", callback_data="cplus HELP_raid")],[InlineKeyboardButton("↺ ʙᴧᴄᴋ ↻", callback_data=f"settingsback_helper"),
     ]]
 
