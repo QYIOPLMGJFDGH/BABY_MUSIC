a5861647f36cdbde49ed2e48a771bf6d0ec79421
UTTAM RATHORE <uk977061@gmail.com>
2024-09-21 12:51:25 +0530

    Update start.py

--- SONALI/utils/inline/start.py
+++ SONALI/utils/inline/start.py
@@ -8,6 +8,18 @@ def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
+                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+            ),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+        ],
+    ]
+    return buttons
+
+
+def private_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
                 text=_["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
