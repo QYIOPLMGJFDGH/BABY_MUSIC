ec9b6f85f7d80a3d08290ac8eee736c651e9a69a
UTTAM RATHORE <uk977061@gmail.com>
2024-09-21 15:18:07 +0530

    Update buttons.py

--- SONALI/utils/stuffs/buttons.py
+++ SONALI/utils/stuffs/buttons.py
@@ -2,12 +2,12 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram import Client, filters, enums 
 
 class BUTTONS(object):
-    BBUTTON = [[InlineKeyboardButton("• ɢꝛᴏᴜᴘ •", callback_data="mplus HELP_Group"),InlineKeyboardButton("• ᴧᴄᴛɪᴏи •", callback_data="mplus HELP_Action"),InlineKeyboardButton("• sᴛɪᴄᴋᴇꝛ •", callback_data="mplus HELP_Sticker")],
-    [InlineKeyboardButton("• ᴛᴧɢ-ᴧʟʟ •", callback_data="mplus HELP_TagAll"),
-    InlineKeyboardButton("• ɪᴍᴘᴏsᴛᴇꝛ •", callback_data="mplus HELP_Imposter"),InlineKeyboardButton("• ᴛ-ᴅᴧꝛᴇ •", callback_data="mplus HELP_TD")],
-    [InlineKeyboardButton("• ʜᴧsʜ-ᴛᴧɢ •", callback_data="mplus HELP_HT")],
-    [InlineKeyboardButton("• ттѕ •", callback_data="mplus HELP_TTS"),
-    InlineKeyboardButton("• ғᴜи •", callback_data="mplus HELP_Fun")],    
+    BBUTTON = [[InlineKeyboardButton("• ɢꝛᴏᴜᴘ •", callback_data="dplus HELP_Group"),InlineKeyboardButton("• ᴧᴄᴛɪᴏи •", callback_data="dplus HELP_Action"),InlineKeyboardButton("• sᴛɪᴄᴋᴇꝛ •", callback_data="dplus HELP_Sticker")],
+    [InlineKeyboardButton("• ᴛᴧɢ-ᴧʟʟ •", callback_data="dplus HELP_TagAll"),
+    InlineKeyboardButton("• ɪᴍᴘᴏsᴛᴇꝛ •", callback_data="dplus HELP_Imposter"),InlineKeyboardButton("• ᴛ-ᴅᴧꝛᴇ •", callback_data="dplus HELP_TD")],
+    [InlineKeyboardButton("• ʜᴧsʜ-ᴛᴧɢ •", callback_data="dplus HELP_HT")],
+    [InlineKeyboardButton("• ттѕ •", callback_data="dplus HELP_TTS"),
+    InlineKeyboardButton("• ғᴜи •", callback_data="dplus HELP_Fun")],    
     [InlineKeyboardButton("↺ ʙᴀᴄᴋ ↻", callback_data=f"modebot_cb"), 
     ]]
     
