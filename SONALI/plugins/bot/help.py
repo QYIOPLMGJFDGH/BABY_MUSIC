b812b8e9c7f8268bc5ba6e6d5a7eb4c9f8e178c8
UTTAM RATHORE <uk977061@gmail.com>
2024-09-21 15:45:15 +0530

    Update help.py

--- SONALI/plugins/bot/help.py
+++ SONALI/plugins/bot/help.py
@@ -91,7 +91,11 @@ async def helper_cb(client, CallbackQuery, _):
 @app.on_callback_query(filters.regex("mbot_cb") & ~BANNED_USERS)
 async def helper_cb(client, CallbackQuery):
     await CallbackQuery.edit_message_text(Helper.HELP_M, reply_markup=InlineKeyboardMarkup(BUTTONS.MBUTTON))
-    
+
+@app.on_callback_query(filters.regex("abot_cb") & ~BANNED_USERS)
+async def helper_cb(client, CallbackQuery):
+    await CallbackQuery.edit_message_text(Helper.HELP_M, reply_markup=InlineKeyboardMarkup(BUTTONS.ABUTTON))
+
 @app.on_callback_query(filters.regex('managebot123'))
 async def on_back_button(client, CallbackQuery):
     callback_data = CallbackQuery.data.strip()
