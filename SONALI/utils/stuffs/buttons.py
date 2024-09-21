ed1e1be49cdd5f82f8f573f0374c80d4e6d9052c
UTTAM RATHORE <uk977061@gmail.com>
2024-09-21 15:07:50 +0530

    Update buttons.py

--- SONALI/utils/stuffs/buttons.py
+++ SONALI/utils/stuffs/buttons.py
@@ -21,5 +21,5 @@ class BUTTONS(object):
     ]]
 
     UBUTTON = [[InlineKeyboardButton("• ᴍᴜsɪᴄ •", callback_data="settings_back_helper"),InlineKeyboardButton("• ᴛᴏᴏʟs •", callback_data=f"mbot_cb")],[InlineKeyboardButton("• ᴍᴀɴᴀɢᴇ •", callback_data=f"bbot_cb"),
-    InlineKeyboardButton("• ʀᴀɪᴅ •", callback_data="mplus HELP_raid")],[InlineKeyboardButton("↺ ʙᴀᴄᴋ ↻", callback_data=f"settingsback_helper"),
+    InlineKeyboardButton("• ʀᴀɪᴅ •", callback_data="cplus HELP_raid")],[InlineKeyboardButton("↺ ʙᴀᴄᴋ ↻", callback_data=f"settingsback_helper"),
     ]]
