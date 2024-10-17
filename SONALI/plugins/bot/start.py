875db194f1110309fdccd531151839a31129ea3f
UTTAM RATHORE <uk977061@gmail.com>
2024-10-17 19:53:10 +0530

    Update start.py

--- SONALI/plugins/bot/start.py
+++ SONALI/plugins/bot/start.py
@@ -33,7 +33,7 @@ async def start_pm(client, message: Message, _):
     typing_message = await message.reply("💗💗💗")  # Initial message
     
     # Simulate typing
-    typing_text = "ᴛʜᴀɴᴋs ғᴏʀ sᴛᴀʀᴛɪɴɢ\nsᴜᴘᴘᴏʀᴛ ᴠ³\nᴄʜᴇᴄᴋ ᴍᴏʀᴇ ғᴜᴛᴜʀᴇs"
+    typing_text = "ᴛʜᴀɴᴋs ғᴏʀ sᴛᴀʀᴛɪɴɢ\nᴍᴜsɪᴄ sᴜᴘᴘᴏʀᴛ ᴠ³\nᴄʜᴇᴄᴋ ᴍᴏʀᴇ ғᴜᴛᴜʀᴇs"
     
     for i in range(1, len(typing_text) + 1):  # Loop through each character
         try:
