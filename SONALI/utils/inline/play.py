7ffb924af0df3403f127406941193a98c12ffca7
UTTAM RATHORE <uk977061@gmail.com>
2024-10-17 22:13:15 +0530

    Update play.py

--- SONALI/utils/inline/play.py
+++ SONALI/utils/inline/play.py
@@ -39,7 +39,7 @@ def track_markup(_, videoid, user_id, channel, fplay):
             InlineKeyboardButton(text="˹ ᴘɾꪮɱꪮᴛιꪮɳ ᴀʋᴀιʅᴀʙʅҽ ˼", callback_data=f"promotext"),
         ],
         [
-            InlineKeyboardButton(text="˹ ⚘ sᴀᴠҽ ᴛʜιs sꪮɳɠ ⚘ ˼", callback_data=f"add_playlist"),
+            InlineKeyboardButton(text="˹ ⚘ ɪ ɯᴀɳᴛ sᴀᴠҽ ᴛʜιs sꪮɳɠ ⚘ ˼", callback_data=f"add_playlist"),
         ],
         [
             InlineKeyboardButton(
