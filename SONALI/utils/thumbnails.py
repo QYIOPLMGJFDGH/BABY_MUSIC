13b6c5f2f4438741f9b2e6ada8ce4f7c0c151e0f
QYIOPLMGJFDGH <Uttamk1578@gmail.com>
2024-12-05 10:48:18 +0530

    Update thumbnails.py

--- SONALI/utils/thumbnails.py
+++ SONALI/utils/thumbnails.py
@@ -150,7 +150,7 @@ async def get_thumb(videoid):
 
     text_x_position = 565
     title1 = truncate(title)
-    draw.text((text_x_position, 180), title1[0], fill=(255, 0, 0), font=title_font)
+    draw.text((text_x_position, 180), title1[0], fill=(255, 255, 255), font=title_font)
     draw.text((text_x_position, 230), title1[1], fill=(255, 0, 0), font=title_font)
     draw.text((text_x_position, 320), f"{channel}  |  {views[:23]}", (255, 0, 0), font=arial)
 
